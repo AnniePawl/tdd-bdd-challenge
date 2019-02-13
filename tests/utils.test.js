29c92940829a08c9c937a6ea5f9c55c3a09c4e74
Anna Pawl <annampawl@gmail.com>
2019-02-13 15:06:36 -0800

    passing area tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -9,6 +9,21 @@ const expect = chai.expect;
 // Lambdas lexically bind this and cannot access the Mocha context.
 // ========================================================
 
+
+// CIRCLE AREA TEST
+// it("should should return the area of a cirle", function() {
+//   const hello = utils.sayHello();
+//   expect(hello).to.be.a("string");
+//   expect(hello).to.equal("Hello");
+//   expect(hello).with.lengthOf(5);
+// });
+
+
+// ========================================================
+// Level 1 Challenges
+// ========================================================
+
+// HELLO TEST
 it("should say hello", function() {
   const hello = utils.sayHello();
   expect(hello).to.be.a("string");
@@ -16,13 +31,29 @@ it("should say hello", function() {
   expect(hello).with.lengthOf(5);
 });
 
-// ========================================================
-// Level 1 Challenges
-// ========================================================
+// RECTANGLE AREA TEST
+it("should return the area of a 5 by 6 rectangle", function() {
+    const rarea = utils.area(5,6);
+    expect(rarea).to.be.an("number");
+    expect(rarea).to.equal(30);
+});
 
-it("should return the area of a 5 by 6 rectangle");
+// CIRCLE AREA TEST
+it("should return the area of a circle of radius 5",
+function() {
+    const carea = utils.circleArea(5);
+    expect(carea).to.be.an("number");
+    expect(carea).to.equal(78.53981633974483);
+});
+// PERIMETER TEST
 
-it("should return the are of a circle of radius 5");
+
+// it("should return the are of a circle of radius 5",
+// function(){
+//     const radius = utils.area();
+//     expect(rarea).to.be.an(int);
+//     expect(rarea).to.equal(30);  expect(hello).with.lengthOf(2);
+// });
 
 // ========================================================
 // Level 2 Challenges
